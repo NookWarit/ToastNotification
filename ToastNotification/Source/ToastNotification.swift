8639785d76a9291611af360ca0b80b3e72a1aac2
NookWarit <>
2021-05-11 00:04:18 +0700

    first commit

---
+++ ToastNotification/Source/ToastNotification.swift
@@ -0,0 +1,21 @@
+//
+//  ToastNotification.swift
+//  ToastNotification
+//
+//  Created by Foodstory on 10/5/2564 BE.
+//
+
+import UIKit
+
+public extension UIViewController {
+    func showToast(message : String, seconds: Double){
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.view.backgroundColor = .black
+        alert.view.alpha = 0.5
+        alert.view.layer.cornerRadius = 15
+        self.present(alert, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
+            alert.dismiss(animated: true)
+        }
+    }
+}
